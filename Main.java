3ccdc89548bc50f2641387f33e43b3906d5cd169
sulthonmufti <msulthonmufti14@gmail.com>
2023-07-17 20:12:27 +0700

    First commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+//NAMA  : MUHAMMAD SULTHON MUFTI
+//NIM   : 2100018213
+//KELAS : D
+
+class Main {
+  public static void main(String[] args) {
+
+    Tictactoe tictactoe = new Tictactoe();
+    
+  }
+}
